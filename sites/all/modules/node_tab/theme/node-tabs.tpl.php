8609b761463a6de2c89b26784856c4cab9b29bd7
fabio <fabio.ger@gmail.com>
2016-07-26 12:00:26 -0300

    Initial Commit

---
+++ sites/all/modules/node_tab/theme/node-tabs.tpl.php
@@ -0,0 +1,12 @@
+<ul class="nav nav-pills">
+  <?php
+  foreach ($tabs as $tab) {
+    if ($tab['active']) {
+      print '<li class="active"><a href="' . $tab['path'] . '" >' . $tab['label'] . '</a></li>';
+    }
+    else {
+      print '<li><a href="' . $tab['path'] . '">' . $tab['label'] . '</a></li>';
+    }
+  }
+?>
+</ul>
